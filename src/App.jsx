831022c65b8995321e5412e1462a8a79adb312b5
mark chhunlong <mark.chhunlong@gmail.com>
2025-03-11 03:19:48 +0000

    refactor: reduce duplicate logic check player

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
+
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = derivedActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((crrActivePlayer) => (crrActivePlayer === "X" ? "O" : "X"));
     setGameTurns((preTurns) => {
-      let currentPlayer = "X";
-      if (preTurns.length > 0 && preTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      let currentPlayer = derivedActivePlayer(preTurns);
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...preTurns,
       ];
 
@@ -48,6 +53,7 @@ function App() {
             turns={gameTurns}
           />
         </div>
+        <Log turns={gameTurns} />
       </main>
     </>
   );
