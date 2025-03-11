5d5c88b81455225e40477c35ec6b0e4e0d553d4a
mark chhunlong <mark.chhunlong@gmail.com>
2025-03-11 03:37:54 +0000

    task: create a winning array combination

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning-combination";
 
 function derivedActivePlayer(gameTurns) {
   let currentPlayer = "X";
